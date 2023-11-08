de9d299a91a108039aa756a325f262c0864be54d
G.Raj Kumar <grajkumar2512@gmail.com>
2023-11-08 12:11:47 +0530

    complete nav and footer

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,15 @@ export default function Home() {
   return (
     <main className="overflow-hidden">
       <Hero />
+
+      <div className="max-width mt-12 padding-x padding-y" id="discover">
+        <div className="home__text-container">
+          <h1 className="font-extrabold text-4xl">Car Catalogue</h1>
+          <p>Explore the cars you might like</p>
+        </div>
+      </div>
+
+      <div className="home__filters"></div>
     </main>
   );
 }
